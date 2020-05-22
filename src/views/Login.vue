3f201df9a55dff1a14e4430548b9e2213b47a9bb
rx-app <rxapp@yandex.com>
2020-05-22 15:51:33 +0800

    return_uri修改

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -129,7 +129,7 @@ export default {
         .replace(/[=]/g, "%3d");
         let reurl='https://www.morninghappy.cn/mh-mall-web-api/auth/wx/receive_code';
         reurl = location.href
-        reurl = 'https://www.morninghappy.cn/sr/index.html#/login'
+        reurl = 'https://www.morninghappy.cn/xz/index.html#/login'
         console.log(reurl)
         // reurl= encodeURI(reurl)
         reurl= encodeURIComponent(reurl)
