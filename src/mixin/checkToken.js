5f27eea7279cfbaeaca0739123b6c9885b96169b
rx-app <rxapp@yandex.com>
2020-05-30 14:41:24 +0800

    mixin token判断修改

--- src/mixin/checkToken.js
+++ src/mixin/checkToken.js
@@ -1,17 +1,28 @@
 let check= {
-    async beforeCreate(){
-        let token = localStorage.getItem('token')
-         const res = await this.$http.get(`auth/check/token/${token}`);
-         if(res.code!=200){
-          
-             localStorage.setItem('url',location.href)
-             var ua = navigator.userAgent.toLowerCase();
-             if(ua.match(/MicroMessenger/i)=="micromessenger") {
-               this.$router.push('/wlogin')
-             } else {
-               this.$router.push('/login')
-             }
-         }
-     }
+  async beforeCreate(){
+      let token = localStorage.getItem('token')
+      if(token){
+       const res = await this.$http.get(`auth/check/token/${token}`);
+       
+       if(!res.data.result_state){
+         
+           localStorage.setItem('url',location.href)
+           var ua = navigator.userAgent.toLowerCase();
+           if(ua.match(/MicroMessenger/i)=="micromessenger") {
+             this.$router.push('/wlogin')
+           } else {
+             this.$router.push('/login')
+           }
+       }
+      }else{
+        localStorage.setItem('url',location.href)
+        var ua = navigator.userAgent.toLowerCase();
+        if(ua.match(/MicroMessenger/i)=="micromessenger") {
+          this.$router.push('/wlogin')
+        } else {
+          this.$router.push('/login')
+        }
+      }
+   }
 }
 export default check
